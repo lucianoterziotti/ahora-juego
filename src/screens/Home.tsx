1d06742304c907a669855e1d653961c7d6053ef4
lucianoterziotti <31636437+lucianoterziotti@users.noreply.github.com>
2024-03-13 16:48:59 -0300

    release-0.1 (#1)
    release-0.1
    
    ---------
    
    Co-authored-by: lupo <lupo.terziotti@gmail.com>

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,28 +4,39 @@ import { useWindowDimensions } from 'react-native';
 import EmptyBoard from '../components/EmptyBoard';
 
 const Home = ({ navigation }) => {
-    const { width } = useWindowDimensions();
-    const boardSize = Math.min(width, 400);
-    return (
-        <SafeAreaView sx={{ display: 'flex', flex: 1, alignItems: 'center' }}>
-            <EmptyBoard size={boardSize} />
-            <Pressable
-                sx={{ padding: 2, backgroundColor: 'black', borderRadius: 4 }}
-                onPress={() => navigation.navigate('Game')}
-            >
-                <Text
-                    sx={{
-                        color: 'white',
-                        textAlign: 'center',
-                        textTransform: 'uppercase',
-                        fontWeight: 500,
-                    }}
-                >
-                    Play Now
-                </Text>
-            </Pressable>
-        </SafeAreaView>
-    );
+  const { width } = useWindowDimensions();
+  const boardSize = Math.min(width, 400);
+  return (
+    <SafeAreaView sx={{ display: "flex", flex: 1, alignItems: "center" }}>
+      <EmptyBoard size={boardSize} />
+      <Pressable
+        sx={{ padding: 2, backgroundColor: "black", borderRadius: 4 }}
+        onPress={() => navigation.navigate("Game")}>
+        <Text
+          sx={{
+            color: "white",
+            textAlign: "center",
+            textTransform: "uppercase",
+            fontWeight: 500
+          }}>
+          Empezar
+        </Text>
+      </Pressable>
+      <Pressable
+        sx={{ padding: 2, backgroundColor: "black", borderRadius: 4 }}
+        onPress={() => navigation.navigate("Welcome")}>
+        <Text
+          sx={{
+            color: "white",
+            textAlign: "center",
+            textTransform: "uppercase",
+            fontWeight: 500
+          }}>
+          Sobre el juego
+        </Text>
+      </Pressable>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
